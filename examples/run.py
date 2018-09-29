612e5d7db7e7784e562eb16117ef83b92ff46417
sdawka <37510491+sdawka@users.noreply.github.com>
2018-09-28 22:06:15 -0700

    Add files via upload

---
+++ examples/run.py
@@ -0,0 +1,37 @@
+import sys
+import time
+import requests
+import Adafruit_MPR121.MPR121 as MPR121
+
+print('Chair monitoring: Start')
+print('Enter IP address of server:')
+ipadd=input()
+ip='http://'+ipadd+'/'
+# Create MPR121 instance.
+cap = MPR121.MPR121()
+
+# Check initialization
+if not cap.begin():
+    print('Error initializing MPR121.  Check your wiring!')
+    requests.get(ip+'error')
+    sys.exit(1)
+
+
+print('Press Ctrl-C to quit.')
+last_touched = cap.touched()
+while True:
+    current_touched = cap.touched()
+    # Check each pin's last and current state to see if it was pressed or released.
+    for i in range(12):
+        # Each pin is represented by a bit in the touched value.  A value of 1
+        # means the pin is being touched, and 0 means it is not being touched.
+        pin_bit = 1 << i
+        # First check if transitioned from not touched to touched.
+        if current_touched & pin_bit and not last_touched & pin_bit:
+            print('{0} touched!'.format(i))
+        # Next check if transitioned from touched to not touched.
+        if not current_touched & pin_bit and last_touched & pin_bit:
+            print('{0} released!'.format(i))
+    # Update last state and wait a short period before repeating.
+    last_touched = current_touched
+    time.sleep(0.1)
